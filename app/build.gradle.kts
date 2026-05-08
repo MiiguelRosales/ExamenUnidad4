a354f034802087dd84fa992e3fbb6a4ad7e1edc5
MiiguelRosales <miiguelangelrv@gmail.com>
2026-05-08 08:50:11 -0600

    new sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.examenunidad4"
-        minSdk = 37
+        minSdk = 34
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
